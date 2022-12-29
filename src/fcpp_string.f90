aa6986e3b172de525c341b4a611a9ea9a60fb6b4
willdickson <wbd@caltech.edu>
2022-12-29 11:02:31 -0800

    Removed unnecessary use in fcpp_string

--- src/fcpp_string.f90
+++ src/fcpp_string.f90
@@ -1,6 +1,5 @@
 module fcpp_string
 
-    use, intrinsic :: iso_c_binding,  only : c_size_t
     use cpp_string_m, only : string_t
     use cpp_string_m, only : char
     use cpp_string_m, only : len
