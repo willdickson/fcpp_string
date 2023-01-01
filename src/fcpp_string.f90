83ac8f18e8a60d582275b53b7bd0c75173864d5f
willdickson <wbd@caltech.edu>
2023-01-01 13:00:56 -0800

    Removed app.

--- src/fcpp_string.f90
+++ src/fcpp_string.f90
@@ -19,32 +19,4 @@ module fcpp_string
     public :: len
     public :: string_fmt
 
-    public :: test
-
-contains
-
-    subroutine test
-        type(string_t) :: s
-        real(wp)       :: rval
-        integer(ip)    :: ival
-        integer        :: i
-        logical        :: flag
-
-        do i = 1, 10
-            rval = i/3.2
-            print *, i, string_fmt('val = {2.3f}', rval)
-        end do
-
-        ival = 10000090 
-        print *, string_fmt('integer = {#x}', ival)
-
-        flag = .true.
-        print *, string_fmt('flag = |{ ^9}|', flag)
-        flag = .false.
-        print *, string_fmt('flag = |{ ^9}|', flag)
-
-        print *, string_fmt('string {_^15}', 'hello')
-
-    end subroutine test
-
 end module fcpp_string
