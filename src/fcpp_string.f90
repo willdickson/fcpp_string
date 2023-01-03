36a9047a28aed41ddd7a3ecd2416151830294edc
willdickson <wbd@caltech.edu>
2023-01-02 18:44:43 -0800

    Removed unneeded use statements at the top of fcpp_string.

--- src/fcpp_string.f90
+++ src/fcpp_string.f90
@@ -1,11 +1,5 @@
 module fcpp_string
 
-    use, intrinsic :: iso_fortran_env, only : wp => real32
-    use, intrinsic :: iso_c_binding,   only : ip => c_int64_t
-    use, intrinsic :: iso_c_binding,   only : c_int32_t
-    use, intrinsic :: iso_c_binding,   only : c_int64_t
-    use, intrinsic :: iso_c_binding,   only : c_size_t
-
     use cpp_string_m, only : string_t
     use cpp_string_m, only : char
     use cpp_string_m, only : len
